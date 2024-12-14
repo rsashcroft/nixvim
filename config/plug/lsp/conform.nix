d68b3cd14bc155da9ff840e9e3a8a6a103f52d87
rsaschroft <robsashcroft@gmail.com>
2024-12-14 23:54:05 +0000

    fix conform issues

--- config/plug/lsp/conform.nix
+++ config/plug/lsp/conform.nix
@@ -5,58 +5,91 @@
       enable = true;
       settings = {
         formatters_by_ft = {
-          bash = [
-            "shellcheck"
-            "shellharden"
-            "shfmt"
+          liquidsoap = [ "liquidsoap-prettier" ];
+          html = [
+            [
+              "prettierd"
+              "prettier"
+            ]
           ];
-          cpp = [ "clang_format" ];
-          javascript = {
-            __unkeyed-1 = "prettierd";
-            __unkeyed-2 = "prettier";
-            timeout_ms = 2000;
-            stop_after_first = true;
-          };
-          "_" = [
-            "squeeze_blanks"
-            "trim_whitespace"
-            "trim_newlines"
+          css = [
+            [
+              "prettierd"
+              "prettier"
+            ]
+          ];
+          javascript = [
+            [
+              "prettierd"
+              "prettier"
+            ]
+          ];
+          javascriptreact = [
+            [
+              "prettierd"
+              "prettier"
+            ]
+          ];
+          typescript = [
+            [
+              "prettierd"
+              "prettier"
+            ]
+          ];
+          typescriptreact = [
+            [
+              "prettierd"
+              "prettier"
+            ]
+          ];
+          python = [ "black" ];
+          lua = [ "stylua" ];
+          nix = [ "nixfmt" ];
+          markdown = [
+            [
+              "prettierd"
+              "prettier"
+            ]
+          ];
+          yaml = [
+            "yamllint"
+            "yamlfmt"
           ];
         };
-        format_on_save = # Lua
-          ''
-            function(bufnr)
-              if vim.g.disable_autoformat or vim.b[bufnr].disable_autoformat then
-                return
-              end
-
-              if slow_format_filetypes[vim.bo[bufnr].filetype] then
-                return
-              end
-
-              local function on_format(err)
-                if err and err:match("timeout$") then
-                  slow_format_filetypes[vim.bo[bufnr].filetype] = true
-                end
-              end
-
-              return { timeout_ms = 200, lsp_fallback = true }, on_format
-             end
-          '';
-        format_after_save = # Lua
-          ''
-            function(bufnr)
-              if vim.g.disable_autoformat or vim.b[bufnr].disable_autoformat then
-                return
-              end
-
-              if not slow_format_filetypes[vim.bo[bufnr].filetype] then
-                return
-              end
-
-              return { lsp_fallback = true }
-            end
-          '';
+        # format_on_save = # Lua
+        #   ''
+        #     function(bufnr)
+        #       if vim.g.disable_autoformat or vim.b[bufnr].disable_autoformat then
+        #         return
+        #       end
+        #
+        #       if slow_format_filetypes[vim.bo[bufnr].filetype] then
+        #         return
+        #       end
+        #
+        #       local function on_format(err)
+        #         if err and err:match("timeout$") then
+        #           slow_format_filetypes[vim.bo[bufnr].filetype] = true
+        #         end
+        #       end
+        #
+        #       return { timeout_ms = 200, lsp_fallback = true }, on_format
+        #      end
+        #   '';
+        # format_after_save = # Lua
+        #   ''
+        #     function(bufnr)
+        #       if vim.g.disable_autoformat or vim.b[bufnr].disable_autoformat then
+        #         return
+        #       end
+        #
+        #       if not slow_format_filetypes[vim.bo[bufnr].filetype] then
+        #         return
+        #       end
+        #
+        #       return { lsp_fallback = true }
+        #     end
+        #   '';
         log_level = "warn";
         notify_on_error = false;
         notify_no_formatters = false;
