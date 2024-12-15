d08b10a6dfff9a697b8b9e77e67028c56cf55020
rsaschroft <robsashcroft@gmail.com>
2024-12-15 00:02:49 +0000

    fix conform

--- config/plug/lsp/conform.nix
+++ config/plug/lsp/conform.nix
@@ -56,40 +56,13 @@
             "yamlfmt"
           ];
         };
-        # format_on_save = # Lua
-        #   ''
-        #     function(bufnr)
-        #       if vim.g.disable_autoformat or vim.b[bufnr].disable_autoformat then
-        #         return
-        #       end
-        #
-        #       if slow_format_filetypes[vim.bo[bufnr].filetype] then
-        #         return
-        #       end
-        #
-        #       local function on_format(err)
-        #         if err and err:match("timeout$") then
-        #           slow_format_filetypes[vim.bo[bufnr].filetype] = true
-        #         end
-        #       end
-        #
-        #       return { timeout_ms = 200, lsp_fallback = true }, on_format
-        #      end
-        #   '';
-        # format_after_save = # Lua
-        #   ''
-        #     function(bufnr)
-        #       if vim.g.disable_autoformat or vim.b[bufnr].disable_autoformat then
-        #         return
-        #       end
-        #
-        #       if not slow_format_filetypes[vim.bo[bufnr].filetype] then
-        #         return
-        #       end
-        #
-        #       return { lsp_fallback = true }
-        #     end
-        #   '';
+        format_on_save = {
+          timeout_ms = 500;
+          lsp_format = "fallback";
+        };
+        # format_after_save = {
+        #   lsp_format = "fallback";
+        # };
         log_level = "warn";
         notify_on_error = false;
         notify_no_formatters = false;
@@ -108,63 +81,5 @@
           };
         };
       };
-
-      # formatOnSave = {
-      #   lspFallback = true;
-      #   timeoutMs = 500;
-      # };
-      # notifyOnError = true;
-      # formattersByFt = {
-      #   liquidsoap = [ "liquidsoap-prettier" ];
-      #   html = [
-      #     [
-      #       "prettierd"
-      #       "prettier"
-      #     ]
-      #   ];
-      #   css = [
-      #     [
-      #       "prettierd"
-      #       "prettier"
-      #     ]
-      #   ];
-      #   javascript = [
-      #     [
-      #       "prettierd"
-      #       "prettier"
-      #     ]
-      #   ];
-      #   javascriptreact = [
-      #     [
-      #       "prettierd"
-      #       "prettier"
-      #     ]
-      #   ];
-      #   typescript = [
-      #     [
-      #       "prettierd"
-      #       "prettier"
-      #     ]
-      #   ];
-      #   typescriptreact = [
-      #     [
-      #       "prettierd"
-      #       "prettier"
-      #     ]
-      #   ];
-      #   python = [ "black" ];
-      #   lua = [ "stylua" ];
-      #   nix = [ "nixfmt" ];
-      #   markdown = [
-      #     [
-      #       "prettierd"
-      #       "prettier"
-      #     ]
-      #   ];
-      #   yaml = [
-      #     "yamllint"
-      #     "yamlfmt"
-      #   ];
-      # };
     };
 }
