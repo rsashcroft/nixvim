807d8eada4849fb33b7b47f23409ab7dcdc4c4c2
rsaschroft <robsashcroft@gmail.com>
2025-02-26 08:06:30 +0000

    another try

--- config/plug/completion/claude.nix
+++ config/plug/completion/claude.nix
@@ -1,21 +1,27 @@
 { pkgs, ... }:
+
 {
-  extraPlugins = [
-    (pkgs.vimUtils.buildVimPlugin {
-      name = "claude";
-      src = pkgs.fetchFromGitHub {
-        owner = "pasky";
-        repo = "claude.vim";
-        rev = "dd7b332c1ecd700abb31d651e4ffbce56c90338f";
-        hash = "04vvscrbdjskc3rkz1n6yqal8az34cbzlinx90iwlzzqwxwqwr7j";
-      };
-    })
-  ];
-  extraConfig = ''
-    let g:claude_api_key = $CLAUDE_API_KEY  
+  plugins = {
+    extraPlugins = [
+      (pkgs.vimUtils.buildVimPlugin {
+        name = "claude";
+        src = pkgs.fetchFromGitHub {
+          owner = "pasky";
+          repo = "claude.vim";
+          rev = "dd7b332c1ecd700abb31d651e4ffbce56c90338f";
+          hash = "04vvscrbdjskc3rkz1n6yqal8az34cbzlinx90iwlzzqwxwqwr7j";
+        };
+      })
+    ];
+  };
+
+  extraConfigVim = ''
+    " Read environment variable into a Vim variable
+    let g:claude_api_key = $CLAUDE_API_KEY
+
+    " Key mappings
     let g:claude_map_implement = "<Leader>ci"
     let g:claude_map_open_chat = "<Leader>cc"
     let g:claude_map_send_chat_message = "<C-]>"
-    let g:claude_map_cancel_response = "<Leader>cx"
   '';
 }
